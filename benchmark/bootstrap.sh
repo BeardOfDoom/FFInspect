2e5599faed8eeff41f6f75fad2b2f76bbc59cfdc
BeardOfDoom <vecsi96@gmail.com>
2019-12-23 00:25:42 +0100

    Added some library installations to the bootstrap script, they are ready for some tests.

--- benchmark/bootstrap.sh
+++ benchmark/bootstrap.sh
@@ -1,24 +1,96 @@
 echo '--------------------------------------------------------------------'
 echo 'START BOOTSTRAP'
 echo '--------------------------------------------------------------------'
+
+echo '--------------------------------------------------------------------'
+echo 'START PREINSTALLS'
+echo '--------------------------------------------------------------------'
+
 sudo apt update
 
-sudo apt install gcc cmake clang g++ -y
+sudo apt install gcc cmake clang g++ gcc-multilib g++-multilib build-essential git libboost-all-dev libgmp3-dev libgmp-dev libssl-dev libprocps-dev pkg-config perl -y
 
-# Check out the library.
-git clone https://github.com/google/benchmark.git
-# Benchmark requires Google Test as a dependency. Add the source tree as a subdirectory.
-git clone https://github.com/google/googletest.git benchmark/googletest
-# Go to the library root directory
-cd benchmark
-# Make a build directory to place the build output.
-mkdir build && cd build
-# Generate a Makefile with cmake.
-# Use cmake -G <generator> to generate a different file type.
-sudo cmake ../
-# Build the library.
-# Use make -j<number_of_parallel_jobs> to speed up the build process, e.g. make -j8 .
+echo '--------------------------------------------------------------------'
+echo 'START MIRACL INSTALL'
+echo '--------------------------------------------------------------------'
+
+#install MIRACL
+git clone https://github.com/miracl/MIRACL.git
+mkdir MIRACLFLAT
+find ./MIRACL -type f -exec mv -f -t ./MIRACLFLAT '{}' +
+cd MIRACLFLAT
+sudo bash linux
+cd ..
+
+echo '--------------------------------------------------------------------'
+echo 'START PARI INSTALL'
+echo '--------------------------------------------------------------------'
+
+#install PARI
+sudo apt install libpari-dev -y
+
+echo '--------------------------------------------------------------------'
+echo 'START LIBFF INSTALL'
+echo '--------------------------------------------------------------------'
+
+#install libff
+git clone https://github.com/scipr-lab/libff.git
+cd libff
+sudo git submodule init && git submodule update
+mkdir build
+cd build
+sudo cmake ..
+sudo make
 sudo make install
+cd ../..
+
+echo '--------------------------------------------------------------------'
+echo 'START FLINT INSTALL'
+echo '--------------------------------------------------------------------'
+
+#install flint
+sudo apt install libflint-dev -y
+
+echo '--------------------------------------------------------------------'
+echo 'START MPFQ INSTALL'
+echo '--------------------------------------------------------------------'
+
+#install mpfq
+wget mpfq.gforge.inria.fr/mpfq-1.1.tar.gz
+tar -xvzf mpfq-1.1.tar.gz
+cd mpfq-1.1
+mkdir build_dir
+cd build_dir
+sudo cmake ../src
+sudo make
+sudo make install
+cd ../..
+
+echo '--------------------------------------------------------------------'
+echo 'START NTL INSTALL'
+echo '--------------------------------------------------------------------'
+
+#install NTL
+wget https://www.shoup.net/ntl/ntl-11.4.1.tar.gz
+tar -xvzf ntl-11.4.1.tar.gz
+cd ntl-11.4.1/src
+sudo bash configure
+sudo make
+sudo make install
+cd ../..
+
+echo '--------------------------------------------------------------------'
+echo 'START GOOGLE BENCHMARK INSTALL'
+echo '--------------------------------------------------------------------'
+
+install google benchmark
+git clone https://github.com/google/benchmark.git
+git clone https://github.com/google/googletest.git benchmark/googletest
+cd benchmark
+mkdir build && cd build
+sudo cmake ../
+sudo make install
+cd ../..
 
 echo '--------------------------------------------------------------------'
 echo 'END BOOTSTRAP'
