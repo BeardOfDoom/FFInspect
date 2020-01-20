f3a3c98e20feb39b3611948be6abdaf007c60bc5
BeardOfDoom <vecsi@DESKTOP-96B284A.localdomain>
2020-01-20 17:26:52 +0100

    Modified the main to be ok for every type of benchmarks.

--- test/main.cc
+++ test/main.cc
@@ -7,50 +7,82 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    if(argc != 2)
+    if(argc != 3 || argc != 4)
     {
-        cout << "Please pass the name of the output file as argument." << endl;
-        cout << "poly_benchmark_data.txt OR poly2_benchmark_data.txt OR prime_benchmark_data.txt" << endl;
+        cout << "The arguments must be the next:" << endl;
+        cout << "First: name of the output (poly_benchmark_data.txt OR poly2_benchmark_data.txt OR prime_benchmark_data.txt)." << endl;
+        cout << "Second: a p prime (characteristic of the field)." << endl;
+        cout << "Third: a q integer (order of the irreducible polynomial defining the field)." << endl;
+        cout << "If q is 0, the program assumes that p is not a prime, but the bitlength of the prime that needs to be generated." << endl;
+
+        return -1;
     }
-    initGenerator();
-    int p = 11;
-    int q = 100;
-    vector<GeneralPolynomialElement> irreduciblePolynomial = generateIrreduciblePolynomial(p, q);
-    vector<GeneralPolynomialElement> polynomialA = generatePolynomialInField(p, q);
-    vector<GeneralPolynomialElement> polynomialB = generatePolynomialInField(p, q);
-    char* scalar = generateNumberOfSize(200);
-    char* exponent = generateNumberOfSize(2000);
-
-    GEN fieldOrder = powuu(p, q);
-    GEN exponentPari = strtoi(exponent);
-    GEN reducedExponentPari = modii(exponentPari, subiu(fieldOrder, 1));
-
-    char* reducedExponent = itostr(reducedExponentPari);
 
     ofstream benchmarkDataFile;
     benchmarkDataFile.open(argv[1]);
 
-    benchmarkDataFile << p << endl;
+    initGenerator();
 
-    benchmarkDataFile << q << endl;
+    char* scalar = generateNumberOfSize(200);
+    char* exponent = generateNumberOfSize(2000);
+    char* reducedExponent;
 
-    for(int i = 0; i < irreduciblePolynomial.size() - 1; i++)
+    int q = atoi(argv[3]);
+    if(q == 0)
     {
-        benchmarkDataFile << irreduciblePolynomial[i].coefficient << " " << irreduciblePolynomial[i].exponent << " ";
-    }
-    benchmarkDataFile << irreduciblePolynomial[irreduciblePolynomial.size() - 1].coefficient << " " << irreduciblePolynomial[irreduciblePolynomial.size() - 1].exponent << endl;
+        char* p = generatePrimeOfSize(atoi(argv[2]));
 
-    for(int i = 0; i < polynomialA.size() - 1; i++)
-    {
-        benchmarkDataFile << polynomialA[i].coefficient << " " << polynomialA[i].exponent << " ";
-    }
-    benchmarkDataFile << polynomialA[polynomialA.size() - 1].coefficient << " " << polynomialA[polynomialA.size() - 1].exponent << endl;
+        GEN numberA = generateNumberInRange(strtoi(p));
+        GEN numberB = generateNumberInRange(strtoi(p));
 
-    for(int i = 0; i < polynomialB.size() - 1; i++)
-    {
-        benchmarkDataFile << polynomialB[i].coefficient << " " << polynomialB[i].exponent << " ";
+        GEN pPari = strtoi(p);
+        GEN reducedExponentPari = modii(exponentPari, subiu(pPari, 1));
+        reducedExponent = itostr(reducedExponentPari);
+
+        benchmarkDataFile << p << endl;
+
+        benchmarkDataFile << q << endl;
+
+        benchmarkDataFile << numberA << endl;
+
+        benchmarkDataFile << numberB << endl;
     }
-    benchmarkDataFile << polynomialB[polynomialB.size() - 1].coefficient << " " << polynomialB[polynomialB.size() - 1].exponent << endl;
+    else
+    {
+        int p = atoi(argv[2]);
+
+        vector<GeneralPolynomialElement> irreduciblePolynomial = generateIrreduciblePolynomial(p, q);
+        vector<GeneralPolynomialElement> polynomialA = generatePolynomialInField(p, q);
+        vector<GeneralPolynomialElement> polynomialB = generatePolynomialInField(p, q);
+
+        GEN fieldOrder = powuu(p, q);
+        GEN exponentPari = strtoi(exponent);
+        GEN reducedExponentPari = modii(exponentPari, subiu(fieldOrder, 1));
+        reducedExponent = itostr(reducedExponentPari);
+
+        benchmarkDataFile << p << endl;
+
+        benchmarkDataFile << q << endl;
+
+        for(int i = 0; i < irreduciblePolynomial.size() - 1; i++)
+        {
+            benchmarkDataFile << irreduciblePolynomial[i].coefficient << " " << irreduciblePolynomial[i].exponent << " ";
+        }
+        benchmarkDataFile << irreduciblePolynomial[irreduciblePolynomial.size() - 1].coefficient << " " << irreduciblePolynomial[irreduciblePolynomial.size() - 1].exponent << endl;
+
+        for(int i = 0; i < polynomialA.size() - 1; i++)
+        {
+            benchmarkDataFile << polynomialA[i].coefficient << " " << polynomialA[i].exponent << " ";
+        }
+        benchmarkDataFile << polynomialA[polynomialA.size() - 1].coefficient << " " << polynomialA[polynomialA.size() - 1].exponent << endl;
+
+        for(int i = 0; i < polynomialB.size() - 1; i++)
+        {
+            benchmarkDataFile << polynomialB[i].coefficient << " " << polynomialB[i].exponent << " ";
+        }
+        benchmarkDataFile << polynomialB[polynomialB.size() - 1].coefficient << " " << polynomialB[polynomialB.size() - 1].exponent << endl;
+
+    }
 
     benchmarkDataFile << scalar << endl;
 
