fcdb783815ae870ca93216f0b831bcf14b16a340
BeardOfDoom <vecsi@DESKTOP-96B284A.localdomain>
2020-01-20 18:18:11 +0100

    Modified the pari poly2 tests.

--- test/pari_benchmark_poly2.cc
+++ test/pari_benchmark_poly2.cc
@@ -2,85 +2,64 @@
 
 #include <pari/pari.h>
 #include <benchmark/benchmark.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 
-static void BM_PolynomField2_Addition(benchmark::State& state, initializer_list<int> firstPolIndexOfOnes, long firstPolDegree, initializer_list<int> secondPolIndexOfOnes, long secondPolDegree)
+using namespace std;
+
+typedef struct GeneralPolynomialElement
 {
-    GEN firstPol = monomial_F2x(firstPolDegree, 0);
-    for(initializer_list<int>::iterator i = firstPolIndexOfOnes.begin(); i < firstPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(firstPol, *i);
-    }
+    unsigned int coefficient;
+    unsigned int exponent;
+} GeneralPolynomialElement;
 
-    GEN secondPol = monomial_F2x(secondPolDegree, 0);
-    for(initializer_list<int>::iterator i = secondPolIndexOfOnes.begin(); i < secondPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(secondPol, *i);
-    }
+int p;
+int q;
+vector<GeneralPolynomialElement> irreduciblePolynomial;
+vector<GeneralPolynomialElement> polynomialA;
+vector<GeneralPolynomialElement> polynomialB;
+string scalar;
+string benchmarkExponent;
+string reducedBenchmarkExponent;
 
+GEN generalPolynomialToPariPolynomial(vector<GeneralPolynomialElement> generalPolynomial)
+{
+    GEN pariPolynomial = monomial_F2x(generalPolynomial.size() - 1, 0);
+    for(int i = 1; i < generalPolynomial.size(); i++)
+    {
+        F2x_set(pariPolynomial, generalPolynomial[i].exponent);
+    }
+    
+    return pariPolynomial;
+}
+
+static void BM_PolynomField2_Addition(benchmark::State& state, GEN firstPol, GEN secondPol)
+{
     for (auto _ : state) {
         F2x_add(firstPol, secondPol);
     }
 }
 
 
-static void BM_PolynomField2_Multiplication(benchmark::State& state, initializer_list<int> firstPolIndexOfOnes, long firstPolDegree, initializer_list<int> secondPolIndexOfOnes, long secondPolDegree, initializer_list<int> modPolIndexOfOnes, long modPolDegree)
+static void BM_PolynomField2_Multiplication(benchmark::State& state, GEN firstPol, GEN secondPol, GEN modPol)
 {
-    GEN firstPol = monomial_F2x(firstPolDegree, 0);
-    for(initializer_list<int>::iterator i = firstPolIndexOfOnes.begin(); i < firstPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(firstPol, *i);
-    }
-
-    GEN secondPol = monomial_F2x(secondPolDegree, 0);
-    for(initializer_list<int>::iterator i = secondPolIndexOfOnes.begin(); i < secondPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(secondPol, *i);
-    }
-
-    GEN modPol = monomial_F2x(modPolDegree, 0);
-    for(initializer_list<int>::iterator i = modPolIndexOfOnes.begin(); i < modPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(modPol, *i);
-    }
-
     for (auto _ : state) {
         F2xq_mul(firstPol, secondPol, modPol);
     }
 }
 
-static void BM_PolynomField2_Exponentiation(benchmark::State& state, initializer_list<int> basePolIndexOfOnes, long basePolDegree, GEN power, initializer_list<int> modPolIndexOfOnes, long modPolDegree)
+static void BM_PolynomField2_Exponentiation(benchmark::State& state,GEN basePol, GEN power, GEN modPol)
 {
-    GEN basePol = monomial_F2x(basePolDegree, 0);
-    for(initializer_list<int>::iterator i = basePolIndexOfOnes.begin(); i < basePolIndexOfOnes.end(); i++)
-    {
-        F2x_set(basePol, *i);
-    }
-
-    GEN modPol = monomial_F2x(modPolDegree, 0);
-    for(initializer_list<int>::iterator i = modPolIndexOfOnes.begin(); i < modPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(modPol, *i);
-    }
-
     for (auto _ : state) {
         F2xq_pow(basePol, power, modPol);
     }
 }
 
-static void BM_PolynomField2_MultiplicativeInversion(benchmark::State& state, initializer_list<int> basePolIndexOfOnes, long basePolDegree, initializer_list<int> modPolIndexOfOnes, long modPolDegree)
+static void BM_PolynomField2_MultiplicativeInversion(benchmark::State& state, GEN basePol, GEN modPol)
 {
-    GEN basePol = monomial_F2x(basePolDegree, 0);
-    for(initializer_list<int>::iterator i = basePolIndexOfOnes.begin(); i < basePolIndexOfOnes.end(); i++)
-    {
-        F2x_set(basePol, *i);
-    }
-
-    GEN modPol = monomial_F2x(modPolDegree, 0);
-    for(initializer_list<int>::iterator i = modPolIndexOfOnes.begin(); i < modPolIndexOfOnes.end(); i++)
-    {
-        F2x_set(modPol, *i);
-    }
-
     for (auto _ : state) {
         F2xq_inv(basePol, modPol);
     }
@@ -89,54 +68,97 @@ static void BM_PolynomField2_MultiplicativeInversion(benchmark::State& state, in
 BENCHMARK_CAPTURE(
     BM_PolynomField2_Addition,
     basic_addition,
-    {200, 69, 42, 0},
-    211,
-    {104, 102, 69, 3, 1},
-    200
+    generalPolynomialToPariPolynomial(polynomialA),
+    generalPolynomialToPariPolynomial(polynomialB)
 );
 
 BENCHMARK_CAPTURE(
     BM_PolynomField2_Multiplication,
     basic_multiplication,
-    {200, 69, 42, 0},
-    211,
-    {104, 102, 69, 3, 1},
-    200,
-    {7, 5, 3, 0},
-    409
+    generalPolynomialToPariPolynomial(polynomialA),
+    generalPolynomialToPariPolynomial(polynomialB),
+    generalPolynomialToPariPolynomial(irreduciblePolynomial)
 );
 
 BENCHMARK_CAPTURE(
     BM_PolynomField2_Exponentiation,
     modular_exponentiation_with_not_reduced_exponent,
-    {104, 102, 69, 3, 1},
-    200,
-    strtoi("114257861865478804407304804720106648610243131623383809889090591194231435391573141592655245498184889136485344775587546688876340744597107535063109170063376118857367666310205539214537693468661910927465064887750418137402612425763591725557531218816269330626202829315598574929746122951434029268750863832704892328653"),
-    {7, 5, 3, 0},
-    409
+    generalPolynomialToPariPolynomial(polynomialA),
+    strtoi(benchmarkExponent.c_str()),
+    generalPolynomialToPariPolynomial(irreduciblePolynomial)
 );
 
 BENCHMARK_CAPTURE(
     BM_PolynomField2_Exponentiation,
     modular_exponentiation_with_reduced_exponent,
-    {104, 102, 69, 3, 1},
-    200,
-    strtoi("545657948216266356582546506665905129604114050233595263203599698308139509840881747025098010154254763608746475177976961773539"),
-    {7, 5, 3, 0},
-    409
+    generalPolynomialToPariPolynomial(polynomialA),
+    strtoi(reducedBenchmarkExponent.c_str()),
+    generalPolynomialToPariPolynomial(irreduciblePolynomial)
 );
 
 BENCHMARK_CAPTURE(
     BM_PolynomField2_MultiplicativeInversion,
     basic_inverse,
-    {104, 102, 69, 3, 1},
-    200,
-    {7, 5, 3, 0},
-    409
+    generalPolynomialToPariPolynomial(polynomialA),
+    generalPolynomialToPariPolynomial(irreduciblePolynomial)
 );
 
 int main(int argc, char** argv)
 {
+    int coefficient, exponent;
+    string tmp, polynomialString;
+    stringstream polynomialStringStream;
+
+    ifstream benchmarkDataFile;
+    benchmarkDataFile.open("poly2_benchmark_data.txt");
+
+    benchmarkDataFile >> p;
+
+    benchmarkDataFile >> q;
+
+    getline(benchmarkDataFile, tmp);
+
+    polynomialString = "";
+    getline(benchmarkDataFile, polynomialString);
+    polynomialStringStream << polynomialString;
+    while(!polynomialStringStream.eof())
+    {
+        polynomialStringStream >> coefficient;
+        polynomialStringStream >> exponent;
+        irreduciblePolynomial.push_back({coefficient, exponent});
+    }
+    polynomialStringStream.clear();
+
+    polynomialString = "";
+    getline(benchmarkDataFile, polynomialString);
+    polynomialStringStream << polynomialString;
+    while(!polynomialStringStream.eof())
+    {
+        polynomialStringStream >> coefficient;
+        polynomialStringStream >> exponent;
+        polynomialA.push_back({coefficient, exponent});
+    }
+    polynomialStringStream.clear();
+
+    polynomialString = "";
+    getline(benchmarkDataFile, polynomialString);
+    polynomialStringStream << polynomialString;
+    while(!polynomialStringStream.eof())
+    {
+        polynomialStringStream >> coefficient;
+        polynomialStringStream >> exponent;
+        polynomialB.push_back({coefficient, exponent});
+    }
+    polynomialStringStream.clear();
+    
+    getline(benchmarkDataFile, scalar);
+
+    getline(benchmarkDataFile, benchmarkExponent);
+
+    getline(benchmarkDataFile, reducedBenchmarkExponent);
+
+    benchmarkDataFile.close();
+
    pari_init(2000000000,0);
    ::benchmark::Initialize(&argc, argv);
    ::benchmark::RunSpecifiedBenchmarks();
